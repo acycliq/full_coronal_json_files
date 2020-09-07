04b1ba203f841f435970db6f371b351822694c8d
Dimitris <dimitris.nicoloutsopoulos@gmail.com>
2020-09-08 00:30:57 +0100

    first commit

---
+++ data/cell_call_demo_data/mouse_full_coronal/cell_type_output/tsv/file_splitter.py
@@ -0,0 +1,52 @@
+import os
+import glob
+
+
+def splitter(filepath, mb_size):
+    '''
+    splits a text file in equally sized parts on the disk. Assumes that there is a header in the first line
+    :param filepath:
+    :param mb_size:
+    :return:
+    '''
+    handle = open(filepath, 'r')
+    OUT_DIR = os.path.join(os.path.splitext(filepath)[0] + '_split')
+
+    if not os.path.exists(OUT_DIR):
+        os.makedirs(OUT_DIR)
+    else:
+        files = glob.glob(OUT_DIR + '/*.*')
+        for f in files:
+            os.remove(f)
+
+    n = 0
+    header_line = next(handle)
+    file_out, handle_out = get_file(OUT_DIR, filepath, n, header_line)
+    for line in handle:
+        size = os.stat(file_out).st_size
+        if size > mb_size*1024*1024:
+            print('saved %s with file size %4.3f MB' % (file_out, size/(1024*1024)))
+            n += 1
+            handle_out.close()
+            file_out, handle_out = get_file(OUT_DIR, filepath, n, header_line)
+        handle_out.write(str(line))
+
+    # print(str(file_out) + " file size = \t" + str(size))
+    print('saved %s with file size %4.3f MB' % (file_out, size / (1024 * 1024)))
+    handle_out.close()
+
+
+def get_file(OUT_DIR, filepath, n, header_line):
+    [filename, ext] = os.path.basename(filepath).split('.')
+    file = os.path.join(OUT_DIR, filename + '_%d.%s' % (n, ext))
+    handle = open(file, "a")
+    handle.write(header_line)
+    return file, handle
+
+
+
+if __name__ == "__main__":
+    mb_size = 99
+    filepaths = [r"geneData.tsv", r"cellData.tsv", r"cellCoords.tsv"]
+    for filepath in filepaths:
+        splitter(filepath, mb_size)
